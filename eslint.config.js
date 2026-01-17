bd6ea2052e1d4c80861357b3af60e20a589ae556
Jean Pool Gutierrez <jeanpoolgg-dev@hotmail.com>
2026-01-17 15:38:01 -0500

    chore(eslint-prettier): integrate eslint with prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
   {
     rules: {
       // Reglas personalizadas

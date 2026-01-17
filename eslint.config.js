4dc6eaf2ee876c213fea1963de38706ba82f8e89
Jean Pool Gutierrez <jeanpoolgg-dev@hotmail.com>
2026-01-17 15:29:56 -0500

    chore(eslint): add eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import tseslint from 'typescript-eslint';
+
+export default [
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // Reglas personalizadas
+    }
+  }
+];

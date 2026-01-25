a698132883cc45f6e91abf45acbaac8d5f1ccf4a
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-01-24 19:33:39 -0500

    feat: fetch product categories

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+import { z } from 'astro:content';
+
+const CategorySchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  slug: z.string(),
+  acf: z.object({
+    icon: z.string(),
+  }),
+});
+
+export const CategoriesSchema = z.array(CategorySchema);
